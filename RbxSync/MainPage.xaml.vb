bd77786c1115ca7cc957f4f76723e4aefaeae91a
Ricardo Loureiro <ricardo@devsparkle.me>
2017-06-20 13:28:02 +0100

    Continued work

--- RbxSync/MainPage.xaml.vb
+++ RbxSync/MainPage.xaml.vb
@@ -1,10 +1,5 @@
-﻿' The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
-
-''' <summary>
-''' An empty page that can be used on its own or navigated to within a Frame.
-''' </summary>
-Public NotInheritable Class MainPage
-    Inherits Page
+﻿Public NotInheritable Class MainPage
+	Inherits Page
 
 	Protected Overrides Sub OnNavigatedTo(e As NavigationEventArgs)
 		ApplicationView.GetForCurrentView().SetPreferredMinSize(New Size(350, 400))
